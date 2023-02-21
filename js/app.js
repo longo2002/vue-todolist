06cdc17c2270f57634c6efd9938a0be6aee7e67b
longo2002 <lombardo.fra2002@gmail.com>
2023-02-21 16:34:11 +0100

    Milestone 1 done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 console.log("works!")
+
+const tasks =[]
+console.log(tasks)
+
+const { createApp } = Vue
+
+createApp({
+	data() {
+		return {
+			tasks: tasks,
+			taskDescription: "",
+            taskDone: false,
+		}
+	},
+	methods: {
+		addTaskBtn() {
+			const newTask = this.taskDescription
+			if(this.taskDescription === "" ){
+                alert("Inserisci una task!")
+            } else {
+                this.tasks.push(newTask)
+            }
+
+			this.taskDescription = ""
+		},
+		removeTaskBtn(i) {
+			console.log(i,this.tasks[i])
+			this.tasks.splice(i, 1)
+		},
+        doneTaskBtn(){
+            this.taskDone = true 
+        }
+
+	},
+}).mount("#app")
